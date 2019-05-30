66a810f44ab69668a7355b3cb3d53b2851b4938e
Nursaid Saidmakhmudov <uzbltd@Nursaids-MacBook-Pro.local>
2019-05-30 15:00:30 -0700

    updated liri.js and added log.txt

--- LIRI.js
+++ LIRI.js
@@ -1,169 +1,210 @@
-
 require("dotenv").config();
 
-const keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
 
-const Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
 
-const rl = require('readline')
+var moment = require('moment');
 
-const axios = require('axios');
 
-const moment = require ('moment');
+var spotify = new Spotify(keys.spotify);
 
-const fs = require ('fs');
+var axios = require("axios");
+var fs = require('fs');
 
-const spotify = new Spotify(keys.spotify);
 
-const validCommands = [
-    'concert-this',
-    'spotify-this-song',
-    'movie-this',
-    'weather-here',
-    'do-what-it-says'
+var liriResponse = process.argv[2];
+var search = process.argv.slice(3).join(" ");
 
-];
+switch (liriResponse) {
+  case "spotify-this-song":
+    spotifySong();
+    break;
 
-const command = process.argv[2];
+  case "movie-this":
+    movieInfo();
+    break;
 
-if (validCommands.indexOf(command)=== -1){
-    log('invalid command\n');
-    return;
+  case "concert-this":
+    bandsInTown();
+    break;
 
-}
-
-let parameter;
-if(command !== 'do-what-it-says'){
-    
-  parameter = process.argv.slice(2).join(' ');
-
-}
-
-function processCommand(command, parameter){
-    switch(command){
-        case 'concert-this':
-             concertThis(parameter);
-            break;
-        case 'spotify-this-song':
-            spotifyThisSong(parameter);
-            break;
-        case 'movie-this':
-            movieThis(parameter);
-            break;
-        case 'weather-here':
-            weatherHere(parameter);
-            break;
-        case 'do-what-it-says':
-            doWhatItSays();
-            break;
-        defailt:
-            log('Cant interpet command\n');
-            return;       
-
-    }
-}
-processCommand(command, parameter);
-
-function concertThis(artist){
-   
-    axios.get(`https://rest.bandsintown.com/artists/${encodeURI(artist)}/events?app_id=${keys.bandsintown.apiKey}`)
-  .then((response) => {
-      const concert = response.data[0];
-      log(`
-      Venue: ${concert.venue.name}
-      City: ${concert.venue.city}, ${concert.venue.region || concert.venue.country}
-      Date: ${moment(concert.datetime).format(MM/DD/YYYY)}\n`);
-  })
-
-  .catch((error) =>{
-      log("error getting concerts", error, '\n');
-  });
-     
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
 };
 
-function spotifyThisSong(song){
-    if(song.length === 0){
-        song = 'Bohemian rhapsody';
+
+function spotifySong() {
+
+  
+  if (!search) {
+    
+    search = "the day that never comes metallica";
+
+    console.log("---> You have not entered any songs, we recommend THE DAY THAT NEVER COMES BY METALLICA!");
+    console.log("---> These are the first three versions. Enjoy");
+
+    console.log("\n-------------\n");
+  } else if (search) {
+
+    
+    console.log("---> These are the first three versions. Enjoy")
+    console.log("---> If you want a specific song try puting the artist name too:")
+    console.log("\n-------------\n");
+  };
+
+  spotify.search({ type: 'track', query: search, limit: 3 }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
     }
-    spotify
-    .search({type: 'track', query: song})
-    .then((response) => {
-        const firstTrack = response.tracks.items[0];
-
-    log(`
-        Artist: $(firstTrack.artists[0].name)
-        Song Name: $(firstTrack.name)
-        Preview Link: ${firstTrack.preview_url || firstTrack.external_urls.spotify}
-        Album: ${firstTrack.album.name}
-       \n`)    
-    })
-
-    .catch((error)=>{
-        log('error getting song info', error, '\n');
-    });
-}
-
-function movieThis(movie){
-    if(movie.legth === 0){
-        movie = "Pulp Fiction"
-    }
-    axios.get(`http://www.omdbapi.com/?t=${encodeURI(movie)}&apikey=${keys.omdbapi.apiKey}`)
+  
+    var trackInfo = data.tracks.items;
    
-    .then((response) =>{
-        response.data 
-    
-        log(`
-    
-        Title: ${response.data.Title}
-        Year: ${response.data.Year}
-        IMDB Rating: ${response.data.Ratings.filter(r => r.Source === 'Internet Movie Database')[0].Value}
-        RT Rating: ${response.data.Ratings.filter(r => r.Source === 'Rotten Tomatoes')[0].Value}
-        Country: ${response.data.Country}
-        Language: ${response.data.Language}
-        Actors: ${response.data.Plot}\n`);
-    })
+    for (var i = 0; i < trackInfo.length; i++) {
 
-   .catch((error)=>{
-       log('error getting movie info', error, '/n');
-   });
+      var artists = trackInfo[i].artists[0].name;
+      var album = trackInfo[i].album.name;
+      var songName = trackInfo[i].name;
+      var previewLink = trackInfo[i].preview_url;
 
-}
-
-function weatherHere(city){
-    if (city.length === 0){
-        city = 'Bellevue'
+      console.log("Artist Name:  " + artists);
+      console.log("Song Name: " + songName);
+      console.log("Album Name:  " + album);
+      console.log("Preview Link: " + previewLink);
+      console.log("\n-------------\n");
     }
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(city)}&appid=${keys.openweather.apiKey}`)
-    .then(response =>{
-        log(`
-        The weather in ${response.data.name} is currently ${response.data.weather[0].description}.
 
-        `)
-    })
-    .catch(err =>{
-        log(`Error getting weather.` , err, '\n');
+  });
 
-    })
-}
 
-function doWhatItSays(){
+};
 
-    const reader = rl.createInterface({
-        input: fs.createReadStream('random.txt')
+function movieInfo() {
+
+  
+  if (!search) {
+    
+    search = "pulp fiction";
+
+    console.log("\n---> If you want you can search for a different movie by its name");
+    console.log("\n---> You have not entered any movies, we recommend Pulp Fiction!\n");
+  } else if (search) {
+   
+    console.log("\n ---> Here is the info for the movie you entered\n");
+  };
+
+  axios.get("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy").then(
+    function (response) {
+     
+
+      console.log("Title of the movie:  " + response.data.Title);
+      console.log("year came out: " + response.data.Year);
+      console.log("IMDB Rating:  " + response.data.Ratings[0].Value);
+      console.log("Rotten Tomatoes:  " + response.data.Ratings[1].Value);
+      console.log("Country:  " + response.data.Country);
+      console.log("Language:  " + response.data.Language);
+      console.log("Plot:  " + response.data.Plot);
+      console.log("Actors: " + response.data.Actors);
+    }).catch(function (error) {
+     
+      console.log(error + " --> something is wrong try again");
+
+    }).finally(function () {
+     
+    });
+};
+
+function bandsInTown() {
+
+  if (!search) {
+    
+    search = "";
+
+    console.log("\n---> You have not entered any artist or Band, We recommend to look some Billie Ellish concerts!");
+  } else if (search) {
+    
+
+  };
+
+  axios.get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=943e8f5b-037a-453f-84bd-a9aa28593784").then(
+    function (response) {
+     
+      var concertInfo = response.data;
+
+      if (concertInfo.length <= 0) {
+        console.log("OHH!!! So sorry seems like we do not have venues for the artist you entered :( Try with another one.");
+      } else {
+        console.log("---> Here is the venues available\n");
+      };
+
+      for (var i = 0; i < concertInfo.length; i++) {
+
+        var artist = concertInfo[i].artist_id;
+        var artistName = concertInfo[i].lineup[0];
+        var venueName = concertInfo[i].venue.name;
+        var country = concertInfo[i].venue.country;
+        var city = concertInfo[i].venue.city;
+        var date = concertInfo[i].datetime;
+        
+        var dateMoment = moment(date).format('MMMM Do YYYY, h:mm:ss a');
+        var tickets = concertInfo[i].offers[0].url;
+
+        console.log("Artist ID:  " + artist);
+        console.log("Artist Name:  " + artistName);
+        console.log("Venue Name:  " + venueName);
+        console.log("Country: " + country);
+        console.log("City:  " + city);
+        console.log("Date: " + dateMoment);
+        console.log("Tickets available here: " + tickets);
+
+        console.log("\n-------------");
+      }
+    }).catch(function (error) {
+      
+      console.log(error + " --> something is wrong try again");
+
+    }).finally(function () {
+     
     });
 
-    reader.on('line', (line) =>{
-        let lineArr =line.split(`,`);
-        const command = lineArr[0];
-        const parameter = lineArr[1].slice(1,-1);
-        processCommand(command, parameter);
-    });
+};
+
+function doWhatItSays() {
+  fs.readFile('random.txt', 'utf8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    data = data.split(',');
+    console.log(data);
+
+    liriResponse = data[0];
+
+    if (liriResponse === "spotify-this-song") {
+      search = (data[1]);
+      spotifySong();
+    } else if (liriResponse === "concert-this") {
+      search = (data[1]);
+      bandsInTown();
+    } else if (liriResponse === "movie-this") {
+      search = (data[1]);
+      movieInfo();
+    }
+  })
 }
 
-function log(message){
-    fs.appendFile('log.txt', message, (err) => {
-        if (err) throw err;
-        console.log(message);
 
-    });
-}
+fs.appendFile("log.txt", liriResponse + " " + search + "\n", function(err) {
+
+
+  if (err) {
+    console.log(err);
+  }
+
+ 
+  else {
+    console.log("Check the log File and you can see your lastes searches!");
+  }
+
+});
--- keys.js
+++ keys.js
@@ -4,15 +4,3 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-
-exports.bandsintown = {
-  apiKey: process.env.BANDSINTOWN_KEY
-};
-
-exports.omdbapi = {
-  apiKey: process.env.OMDBAPI_KEY
-};
-
-exports.openweather = {
-  apiKey: process.env.OPENWEATHER_KEY
-};
